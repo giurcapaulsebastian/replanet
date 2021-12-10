a51f21597d5f5e9b287190d01e717621234b7caa
Sebastian Giurca <giurcapaulsebastian@gmail.com>
2021-12-10 09:38:38 +0200

    [U] Integrated react into app succesfully

--- replanet/frontend/src/components/App.js
+++ replanet/frontend/src/components/App.js
@@ -1,10 +1,45 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 
-class App extends Component{
+import Header from './layout/Header';
+
+import {
+    GoogleMap,
+    useLoadScript,
+    Marker,
+    InfoWindow,
+} from "google-maps-react";
+
+import { formatRelative } from "date-fns";
+
+
+const libraries = ["places"];
+
+// export default function App(){
+//     const {isLoaded, loadError} = useLoadScript({
+//         googleMapsApiKey : process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+//         libraries,
+//     })
+
+//     if (loadError) return "Error loading maps";
+//     if (!isLoaded) return "Loading Maps";
+
+//     return <div>map</div>
+// }
+
+console.log("TE ROG AFISEAZA LOG");
+
+class App extends Component {
     render(){
-        return <h1>React App</h1>
+        return(
+            <Header></Header>
+        );
     }
+
+    // render(){
+    //     console.log("Rendering"),
+    //     <Header></Header>
+    // }
 }
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App></App>, document.getElementById('app'));
